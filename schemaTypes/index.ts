8032f3f25d3d59ed66bd5c5b5d298780b7001b11
Skaps664 <msudaisk664@gmail.com.com>
2025-07-24 19:40:01 +0500

    new schema adde

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,8 +1,7 @@
 import homePageContent from "./homePageContent";
-import homePageAds from "./homePageAds";
 import homePageBanner from "./homePageBanner";
 import solarExpressBanner from "./homePageSEBanner";
 import tickerContent from "./tickerContent";
 import solarAds from "./solarAds";
 
-export const schemaTypes = [homePageContent, homePageAds, homePageBanner, solarExpressBanner, tickerContent, solarAds] 
+export const schemaTypes = [homePageContent, homePageBanner, solarExpressBanner, tickerContent, solarAds] 
