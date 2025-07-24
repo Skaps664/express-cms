f2e471995cd9990133f912b715fd1f2735b98008
Skaps664 <msudaisk664@gmail.com.com>
2025-07-24 11:15:57 +0500

    ticker schema added

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,5 +2,6 @@ import homePageContent from "./homePageContent";
 import homePageAds from "./homePageAds";
 import homePageBanner from "./homePageBanner";
 import solarExpressBanner from "./homePageSEBanner";
+import tickerContent from "./tickerContent";
 
-export const schemaTypes = [homePageContent, homePageAds, homePageBanner, solarExpressBanner] 
+export const schemaTypes = [homePageContent, homePageAds, homePageBanner, solarExpressBanner, tickerContent] 
---
+++ schemaTypes/tickerContent.ts
@@ -0,0 +1,50 @@
+const tickerContent = {
+  name: 'tickerContent',
+  title: 'Ticker Content',
+  type: 'document',
+  fields: [
+    {
+      name: 'page',
+      title: 'Page',
+      type: 'string',
+    },
+    {
+      name: 'one',
+      title: 'one',
+      type: 'string',
+    },
+    {
+      name: 'two',
+      title: 'two',
+      type: 'string',
+    },
+    {
+      name: 'three',
+      title: 'three',
+      type: 'string',
+    },
+    {
+      name: 'four',
+      title: 'four',
+      type: 'string',
+    },
+    {
+      name: 'five',
+      title: 'five',
+      type: 'string',
+    },
+    {
+      name: 'six',
+      title: 'six',
+      type: 'string',
+    },
+    {
+      name: 'seven',
+      title: 'seven',
+      type: 'string',
+    },
+
+  ],
+}
+
+export default tickerContent;
